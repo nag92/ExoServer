d604db2b095663bac0a3b0b8cbd40dcde9cee627
Nathaniel Goldfarb <nagoldfarb@wpi.edu>
2019-03-31 13:11:00 -0400

    EXO-351 add propeties to the kalman filter so that the A and B matrix can be updated on the file. This will allow for the time step to be updated.

--- Filters/Kalman.py
+++ Filters/Kalman.py
@@ -11,36 +11,50 @@ import numpy as np
 class kalman_filter:
     # take in all the parameters of the linear kalman filter
     def __init__(self, A, B, C, Q, P, R, x):
-        self.A = A  # state trasition matrix
-        self.B = B  # control matrix
-        self.C = C  # measurement model
-        self.Q = Q  # proccess noise covariance
-        self.P = P  # predeciton
-        self.R = R  ##measurement noise covariance
-        self.state = x
+        self._A = A  # state trasition matrix
+        self._B = B  # control matrix
+        self._C = C  # measurement model
+        self._Q = Q  # proccess noise covariance
+        self._P = P  # predeciton
+        self._R = R  ##measurement noise covariance
+        self._state = x
 
     # move to the  next position
     def move(self, u, z):
-        self.predict(u)
-        self.update(z)
+        self.__predict(u)
+        self.__update(z)
 
     # get the current state
     def getState(self):
         return self.state
 
     # predict the next state
-    def predict(self, u):
-        self.state = self.A * self.state + self.B * u
-        self.P = self.A * self.P * self.A.T + self.Q
+    def __predict(self, u):
+        self.state = self._A * self.state + self._B * u
+        self.P = self._A * self._P * self._A.T + self._Q
 
     # update the model
-    def update(self, z):
+    def __update(self, z):
         # find the kalman gain
-        K = self.P * (self.C.T) * np.linalg.inv((self.C * self.P * (self.C.T) + self.R))
+        K = self.P * (self._C.T) * np.linalg.inv((self._C * self.P * (self._C.T) + self._R))
         # get the current state
-        self.state = self.state + K * (z - self.C * self.state)
+        self.state = self.state + K * (z - self._C * self.state)
         # get the next prediction
         size = self.state.shape[0]
-        self.P = (np.eye(size) - K * self.C) * self.P
+        self.P = (np.eye(size) - K * self._C) * self.P
 
+    @property
+    def A(self):
+        return self._A
 
+    @A.setter
+    def A(self, value):
+        self._A = value
+
+    @property
+    def B(self, value):
+        return self._B
+
+    @B.setter
+    def B(self, value):
+        self._B = value
