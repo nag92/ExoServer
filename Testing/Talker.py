ee58261e2fc90c743634518d0c82a3695f01a532
nathaniel <nagoldfarb@wpi.edu>
2019-04-13 08:25:33 -0400

    working on talker

--- Testing/Talker.py
+++ Testing/Talker.py
@@ -1,27 +1,48 @@
-import binascii
+# import binascii
+# import socket
+# import struct
+# import sys
+#
+# # Create a TCP/IP socket
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server_address = ('localhost', 1234)
+# sock.bind(server_address)
+# sock.listen(1)
+#
+# while True:
+#     MESSAGE = b'\x09\x00\x09'
+#     sock.send(MESSAGE)
+#     print >> sys.stderr, '\nwaiting for a connection'
+#     connection, client_address = sock.accept()
+#     try:
+#        pass
+#
+#     finally:
+#         connection.close()
 import socket
-import struct
-import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 10000)
-sock.bind(server_address)
-sock.listen(1)
+host = ''  # Symbolic name meaning all available interfaces
+port = 12345  # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
 
-unpacker = struct.Struct('I 2s f')
-
+print host, port
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
 while True:
-    MESSAGE = b"000010010000000000001001"
-    sock.send(MESSAGE)
-    print >> sys.stderr, '\nwaiting for a connection'
-    connection, client_address = sock.accept()
+
     try:
-        data = connection.recv(unpacker.size)
-        print >> sys.stderr, 'received "%s"' % binascii.hexlify(data)
+        data = conn.recv(2048)
 
-        unpacked_data = unpacker.unpack(data)
-        print >> sys.stderr, 'unpacked:', unpacked_data
+        if not data: break
 
-    finally:
-        connection.close()
+        print "Client Says: " + data
+        MESSAGE = b'\x09\x00\x09'
+        conn.sendall(MESSAGE)
+
+    except socket.error:
+        print "Error Occured."
+        break
+
+conn.close()
