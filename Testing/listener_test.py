5696a8cd53312bc23e8c7365b28fd287295d769b
nathaniel <nagoldfarb@wpi.edu>
2019-04-22 23:05:39 -0400

    EXO-362 strangly working, still needs some work

---
+++ Testing/listener_test.py
@@ -0,0 +1,21 @@
+from Communication import Ethernet
+from Managers import SensorManager, PlotManager, FilterManager
+from Robot import Robot
+
+path = "/home/nathaniel/git/exoserver/Config/sensor_list.yaml"
+comm = Ethernet.Ethernet()
+SM = SensorManager.SensorManager()
+FM = FilterManager.FilterManager()
+SM.register_sub(FM)
+plotter = PlotManager.PlotManager()
+robot = Robot.Robot(path, SM, FM)
+
+comm.register_sub(SM)
+
+print "\n\n"
+
+comm.start()
+
+while 1:
+    for name, sensor in SM.get_sensors().iteritems():
+        print sensor.name, sensor.get_values()
