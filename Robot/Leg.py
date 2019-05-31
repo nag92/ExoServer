3c0fdc25e693a65c08036da8da1a491c18e75c98
nathaniel <nagoldfarb@wpi.edu>
2019-05-31 10:21:49 -0400

    EXO-381 calculate the CoP of the FSR sensors and set the x and y location to a propery

--- Robot/Leg.py
+++ Robot/Leg.py
@@ -13,6 +13,26 @@ class Leg(object):
         self._hip = hip
         self._knee = knee
         self._ankle = ankle
+        self._CoP = [0, 0]
+
+    @property
+    def CoP(self):
+        return self._CoP
+
+    @CoP.setter
+    def CoP(self, value):
+        self._CoP = value
 
     def calc_CoP(self):
         fsrs = self._ankle.FSRs
+
+        total_force = 0
+        centerX = 0
+        centerY = 0
+
+        for sensor in fsrs:
+            total_force += sensor.get_values()
+            centerX += sensor.get_values() * sensor.orientation[0]
+            centerY += sensor.get_values() * sensor.orientation[1]
+
+        self._CoP = [centerX / total_force, centerY / total_force]
