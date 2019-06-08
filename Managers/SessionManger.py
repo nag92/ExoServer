64dbf7ab959d02aafc5fb198de2204c4efad262a
nathaniel <nagoldfarb@wpi.edu>
2019-06-08 16:46:58 -0400

    working on session manager to handle the backend of the GUI. It will respond to the front end GUI promts.

---
+++ Managers/SessionManger.py
@@ -0,0 +1,103 @@
+import datetime
+from PyQt5 import QtWidgets
+
+import yaml
+from typing import List, Any, Dict
+
+from Communication import Ethernet
+from Managers import Manager, RecorderManager, FilterManager, SensorManager, PlotManager
+from Robot import Robot
+
+
+class SessionManager(Manager.Manager):
+    btns = None  # type: Dict[Any, QtWidgets.QAbstractButton]
+    # self.textbox.setText("")
+    text_boxes = None  # type: List[QtWidgets.QPlainTextEdit]
+
+    def __int__(self):
+        path = "/home/nathaniel/git/exoserver/Config/sensor_list.yaml"
+        self.SM = SensorManager.SensorManager()
+        self.FM = FilterManager.FilterManager()
+
+        self.SM.register_sub(self.FM)
+
+        plotter = PlotManager.PlotManager()
+        robot = Robot.Robot(path, self.SM, self.FM)
+        self.sensor_names = self.SM.get_sensor_names()
+        comm = Ethernet.Ethernet()
+        comm.register_sub(self.SM)
+        self.SM.register_sub(plotter)
+        self.recorder = RecorderManager.RecorderManager(self.sensor_names)
+        self.SM.register_sub(self.recorder)
+        self.in_session = False
+        self.txt_boxes = []
+        self.btns = {}
+        self.trial_number = 0
+        self.session_name = ""
+        self.date = datetime.datetime.now()
+
+        pass
+
+    def on_click(self, btn):
+        """
+        General callback that will handle the function calls to the other
+        fuctions. This will make it easier to build the front end because
+        only one callback in the front end.
+        :param btn:
+        :return:
+        """
+        pass
+
+    def session_callback(self):
+        session = {}
+        for box in self.text_boxes:
+            name = box.objectName()
+            value = box.textbox.text()
+
+            # Check if the box has a value
+            if value is "":
+                RuntimeError("missing value")
+                return
+            else:
+                session[name] = value
+
+        session["date"] = self.date
+
+        self.session_name = "subject_" + str(session["subject"])
+
+        with open(self.session_name + '.yaml', 'w') as outfile:
+            yaml.dump(session, outfile, default_flow_style=False)
+
+        self.in_session = True
+
+        pass
+
+    def record_callback(self, button):
+
+        if not self.in_session:
+            print "Session not started"
+            return
+        if self.recorder.recording:
+            print "In the middle of recording. Stop the trial first then you can start again"
+            return
+
+        trial_name = "trial_" + str(self.trial_number)
+        self.trial_number += 1
+        self.btns["btnRecording"].setStyleSheet("background-color: red")
+        self.recorder.new_file(trial_name)
+        self.recorder.start_recording()
+
+    def monitor_callback(self, button):
+        """
+        Bring up the monitors
+        :param button:
+        :return:
+        """
+        pass
+
+    def stop_callback(self, button):
+        self.recorder.stop_recording()
+        pass
+
+    def record_callback(self, button):
+        pass
